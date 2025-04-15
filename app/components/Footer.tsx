4e117792a1cf883ae12cb381b2d7874439bd035c
YheChen <yanzhenchen123@gmail.com>
2025-04-14 22:42:19 -0400

    centered footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="border-t py-6">
-      <div className="container flex justify-center items-center">
+      <div className="mx-auto max-w-7xl w-full flex justify-center items-center px-4">
         <p className="text-center text-sm leading-loose text-muted-foreground">
           © {new Date().getFullYear()} Yanzhen Chen
         </p>
