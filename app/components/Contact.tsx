e7a9c2b7d126d8817f213ee50126f8ea6f913070
YheChen <yanzhenchen123@gmail.com>
2025-04-16 12:16:13 -0400

    removed phone number

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -151,7 +151,7 @@ export default function Contact() {
                     linkedin.com/in/yanzhenchen
                   </Link>
                 </div>
-                <div className="flex items-center gap-3">
+                {/* <div className="flex items-center gap-3">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5 text-muted-foreground"
@@ -163,7 +163,7 @@ export default function Contact() {
                     <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z" />
                   </svg>
                   <span>(825) 747-1417</span>
-                </div>
+                </div> */}
               </div>
               <div className="mt-6">
                 <h4 className="text-sm font-medium">Location</h4>
